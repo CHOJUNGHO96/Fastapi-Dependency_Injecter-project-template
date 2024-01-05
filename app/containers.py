776dea4ef32d4f1708ca23d082840bf698d6c070
jhcho <cho418651@ssrinc.co.kr>
2024-01-05 15:53:17 +0900

    .

--- app/containers.py
+++ app/containers.py
@@ -4,8 +4,7 @@ from dependency_injector import containers, providers
 from fastapi.requests import Request
 
 from app.apis.v1.auth.login.containers import Container as LoginContainer
-from app.apis.v1.auth.registration.containers import \
-    Container as RegistrationContainer
+from app.apis.v1.auth.registration.containers import Container as RegistrationContainer
 from app.common.config import get_config
 from app.database.conn import Database
 from app.database.redis_config import RedisConfig
@@ -25,10 +24,7 @@ class Container(containers.DeclarativeContainer):
     logging = providers.Singleton(LogAdapter, request=Request, response=None, error=None)
 
     # db 의존성 주입
-    if os.environ.get("API_MODE") == "test":
-        db = providers.Factory(Database, conf=config)
-    else:
-        db = providers.Singleton(Database, conf=config)
+    db = providers.Singleton(Database, conf=config)
 
     # Redis 의존성 주입
     redis = providers.Singleton(RedisConfig, conf=config)
