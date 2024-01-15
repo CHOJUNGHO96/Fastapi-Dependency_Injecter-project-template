c841d6fcb24a309f4694f96ee58ab9477a7be44a
CHOJUNGHO96 <jo4186@naver.com>
2024-01-15 22:18:08 +0900

    test code news_list put, delete 추가

--- app/tests/test_news.py
+++ app/tests/test_news.py
@@ -16,11 +16,37 @@ async def test_post_news_list(client: TestClient):
         },
     )
     assert response.status_code == 200
+    response_body = response.json()
+    client.app_state.update({"article_id": response_body["list"][0]["article_id"]})
+
+
+@pytest.mark.asyncio
+async def test_put_news_list(client: TestClient):
+    response = client.put(
+        url="news/list",
+        json={
+            "title": "test_title2",
+            "article_id": client.app_state["article_id"],
+        },
+    )
+    assert response.status_code == 200
 
 
 @pytest.mark.asyncio
 async def test_get_news_list(client: TestClient):
     response = client.get(
         url="news/list",
+        params={"title": "test_title2", "article_id": client.app_state["article_id"]},
+    )
+    response_body = response.json()
+    assert response.status_code == 200
+    assert response_body["list"][0]["article_id"] == client.app_state["article_id"]
+
+
+@pytest.mark.asyncio
+async def test_delete_news_list(client: TestClient):
+    response = client.delete(
+        url="news/list",
+        params={"article_id": client.app_state["article_id"]},
     )
     assert response.status_code == 200
