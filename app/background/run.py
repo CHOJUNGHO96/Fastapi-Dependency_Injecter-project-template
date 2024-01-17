3e4a307bc8e3d7add4ea953acb75036a0e28283a
CHOJUNGHO96 <jo4186@naver.com>
2024-01-17 18:42:09 +0900

    .

--- app/background/run.py
+++ app/background/run.py
@@ -8,6 +8,5 @@ celery_app = container.celery_app()
 
 @celery_app.task
 def run_news_crawling():
-    _container = BackgroundContainer()
-    news_crawling = _container.celery_news_crawling()
+    news_crawling = container.celery_news_crawling()
     asyncio.run(news_crawling.get_news_crawling_to_insert())
