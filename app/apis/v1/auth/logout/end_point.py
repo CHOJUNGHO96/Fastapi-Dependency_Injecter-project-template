d3d3480310e3380aa8ed93e254e2f3e1a3d7e5de
CHOJUNGHO96 <jo4186@naver.com>
2024-02-20 12:26:02 +0900

    로그아웃 API추가

---
+++ app/apis/v1/auth/logout/end_point.py
@@ -0,0 +1,15 @@
+from fastapi import APIRouter
+from fastapi.responses import JSONResponse
+
+router = APIRouter()
+
+
+@router.post("/logout")
+async def post_login():
+    """
+    `로그아웃  API`
+    """
+    response = JSONResponse(content={"message": "Suceess Logout."})
+    response.delete_cookie(key="token_type")
+    response.delete_cookie(key="access_token")
+    return response
