9ce3548654c23ae5a2174bf1fcefb02b3ddc5560
CHOJUNGHO96 <jo4186@naver.com>
2024-02-21 14:54:31 +0900

    필요없는 코드 제거

--- app/database/conn.py
+++ app/database/conn.py
@@ -3,9 +3,6 @@ from typing import AsyncIterator
 
 from sqlalchemy.ext.asyncio import (AsyncSession, async_sessionmaker,
                                     create_async_engine)
-from sqlalchemy.ext.declarative import declarative_base
-
-Base = declarative_base()
 
 
 class Database:
